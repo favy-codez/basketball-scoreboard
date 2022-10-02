f5b6b5c39ab973c3482adae0c44793bb6c3118e2
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-10-02 18:23:52 +0100

    commit message

--- index.js
+++ index.js
@@ -4,38 +4,61 @@ const periodNum =document.getElementById("period-num")
 
 let homebox= 0
 let guestbox = 0
+let periodNo = 1
 
 function add1point() {
     homebox += 1
     homeboxEl.textContent = homebox
+    homeboxEl.style.color = "white"
 }
 
 function add2point() {
     homebox += 2
     homeboxEl.textContent = homebox
+    homeboxEl.style.color = "white"
 }
 
 function add3point() {
     homebox += 3
     homeboxEl.textContent = homebox
+    homeboxEl.style.color = "white"
 }
 
+// period section
+function period(){
+    if(periodNo <= 3){
+        periodNo += 1
+        periodNum.textContent = periodNo
+    }else {
+        
+    }
+}
+
+// guest section
 function add1points() {
     guestbox += 1
     guestboxEl.textContent = guestbox
+    guestboxEl.style.color = "white"
 }
 
 function add2points(){
     guestbox += 2
     guestboxEl.textContent = guestbox
+    guestboxEl.style.color = "white"
 }
 
 function add3points(){
     guestbox += 3
     guestboxEl.textContent = guestbox
+    guestboxEl.style.color = "white"
 }
 
 function reset() {
     homeboxEl.textContent = 0
+    homeboxEl.style.color = "black"
     guestboxEl.textContent = 0
+    guestboxEl.style.color = "black"
+    periodNum.textContent = 1
 }
+
+//  color section
